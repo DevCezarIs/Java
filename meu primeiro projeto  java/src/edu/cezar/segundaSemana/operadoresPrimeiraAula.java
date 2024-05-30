a517cb15a604ea564bfa0fd0c4970188853715bf
DevCezarIs <cezar.dev24@gmail.com>
2024-05-30 12:11:29 -0300

    Commit basico

---
+++ meu primeiro projeto  java/src/edu/cezar/segundaSemana/operadoresPrimeiraAula.java
@@ -0,0 +1,19 @@
+package edu.cezar.segundaSemana;
+ public class operadoresPrimeiraAula  { 
+    public static void main(String[] args){
+       int numero = 5;
+       numero = - numero;
+
+       System.out.println(numero);
+
+       numero = - numero;
+       System.out.println(numero);
+
+    
+
+    }
+
+
+ }
+
+ 
---
+++ meu primeiro projeto  java/src/edu/cezar/segundaSemana/operadoresSegundaAula.java
@@ -0,0 +1,16 @@
+package edu.cezar.segundaSemana;
+ public class operadoresSegundaAula  { 
+    public static void main(String[] args){
+         int numero = 5;
+
+         System.out.println(-- numero);
+         System.out.println(numero);
+
+    
+
+    }
+
+
+ }
+
+ 

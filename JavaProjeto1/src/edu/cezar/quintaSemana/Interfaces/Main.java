01a6b6a91e519b031e58c0ffe6f540d0b4a65b19
DevCezarIs <cezar.dev24@gmail.com>
2025-09-15 09:14:48 -0300

    Utilizando Interfaces no Java

--- JavaProjeto1/src/edu/cezar/quintaSemana/Interfaces/Main.java
+++ JavaProjeto1/src/edu/cezar/quintaSemana/Interfaces/Main.java
@@ -1,4 +1,8 @@
 package edu.cezar.quintaSemana.Interfaces;
 
 public class Main {
+    public static void main(String[] args) {
+        Kia cerato = new Kia();
+        cerato.acelerar();
+    }
 }

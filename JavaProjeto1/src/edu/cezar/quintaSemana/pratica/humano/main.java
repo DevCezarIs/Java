ab0efbc3ca96e55eb93513f62c390d59becd324e
DevCezarIs <cezar.dev24@gmail.com>
2025-09-16 09:26:09 -0300

    treinando pt1

---
+++ JavaProjeto1/src/edu/cezar/quintaSemana/pratica/humano/main.java
@@ -0,0 +1,9 @@
+package edu.cezar.quintaSemana.pratica.humano;
+
+public class main {
+    public static void main(String[] args) {
+        Humano cezar = new Humano("Cezar", "Humano", 20, 0);
+
+
+    }
+}
